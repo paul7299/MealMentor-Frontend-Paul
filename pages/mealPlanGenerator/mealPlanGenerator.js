44881b23b51547ad4a97900eaadb67d5a7f6d738
mathiasfri <mathiasfridthjof@live.dk>
2023-11-07 12:05:07 +0100

    preferences added

--- pages/mealPlanGenerator/mealPlanGenerator.js
+++ pages/mealPlanGenerator/mealPlanGenerator.js
@@ -11,3 +11,16 @@ export async function initMealPlanGenerator() {
 
 }
 
+
+document.addEventListener("DOMContentLoaded", function() {
+  const itemList = document.getElementById("preference-list");
+  const addItemButton = document.getElementById("add-preference");
+
+  addItemButton.addEventListener("click", function() {
+      const inputField = document.createElement("input");
+      inputField.type = "text";
+      inputField.className = "form-control";
+
+      itemList.appendChild(inputField);
+  });
+});
