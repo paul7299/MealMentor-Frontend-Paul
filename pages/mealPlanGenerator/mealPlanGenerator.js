24f648a6d8af95e3d26b9206fe1d3d82e6950645
paul7299 <paul7299@stud.kea.dk>
2023-11-08 09:03:49 +0100

    1st

--- pages/mealPlanGenerator/mealPlanGenerator.js
+++ pages/mealPlanGenerator/mealPlanGenerator.js
@@ -5,10 +5,31 @@ import {
   makeOptions,
 } from "../../utils.js";
 
-const URL = API_URL + "/mealPlanGenerator";
+const SERVER_URL = API_URL + "/mealPlanGenerator";
 
 export async function initMealPlanGenerator() {
+  const aboutValue = document.getElementById('about').value;
+  const mealTypeValue = document.getElementById('mealType').value;// Get the selected value from the dropdown
+  const inputValues =  mealTypeValue + " " + aboutValue;
+  console.log("meatype: " + mealTypeValue)
+  console.log(inputValues)
+  const URL1 = `${SERVER_URL}?mealType=${mealTypeValue}&about=${aboutValue}`;// Include the dropdown value in the "about" parameter
+  const URL = `${SERVER_URL}?about=${inputValues}`;
+  const spinner = document.getElementById('spinner1');
+  const result = document.getElementById('result');
+  result.style.color = "black";
 
+  try {
+    spinner.style.display = "block";
+    const response = await fetch(URL).then(handleHttpErrors);
+    document.getElementById('result').innerText = response.answer;
+    //document.getElementById('about').value = '';
+  } catch (e) {
+    result.style.color = "red";
+    result.innerText = e.message;
+  } finally {
+    spinner.style.display = "none";
+  }
 }
 
 
