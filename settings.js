d5cd8581699d34c783e547e26676ea06eda44fae
mathiasfri <mathiasfridthjof@live.dk>
2023-11-10 15:32:10 +0100

    added https to backend link

--- settings.js
+++ settings.js
@@ -1,3 +1,3 @@
-export const API_URL = "mealmentor-backend.azurewebsites.net/api" // Azure: http://localhost:8080/api
+export const API_URL = "https://mealmentor-backend.azurewebsites.net/api" // Azure: http://localhost:8080/api
 
 export const FETCH_NO_API_ERROR = " (Is the API online or did the endpoint exists ?)"
