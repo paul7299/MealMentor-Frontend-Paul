598289a9a0a101cf73917a5d013580553f069b8e
paul7299 <paul7299@stud.kea.dk>
2023-11-10 14:53:23 +0100

    api url is now azure backend

--- settings.js
+++ settings.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:8080/api" // Azure: mealmentor-backend.azurewebsites.net
+export const API_URL = "mealmentor-backend.azurewebsites.net/api" // Azure: http://localhost:8080/api
 
 export const FETCH_NO_API_ERROR = " (Is the API online or did the endpoint exists ?)"
