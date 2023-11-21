323a82e6028042704e766ff2ef4936146a32bc70
Paul <paul7299@stud.kea.dk>
2023-11-21 10:20:29 +0100

    db picker in settings

--- settings.js
+++ settings.js
@@ -1,3 +1,14 @@
-export const API_URL = "https://mealmentor-backend.azurewebsites.net/api" // Azure: http://localhost:8080/api
+// export const API_URL = "https://mealmentor-backend.azurewebsites.net/api" // Azure: http://localhost:8080/api
+
+
+export let API_URL = ""
+
+if (window.location.hostname === 'localhost' || window.location.hostname === "127.0.0.1") {
+  API_URL = "http://localhost:8080/api"
+} else{
+  //Add URL to your hosted API, once you have it deployed.
+  API_URL = "https://mealmentor-backend.azurewebsites.net/api"
+}
+
 
 export const FETCH_NO_API_ERROR = " (Is the API online or did the endpoint exists ?)"
