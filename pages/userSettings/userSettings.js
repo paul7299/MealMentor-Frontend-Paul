dc451360dd4f672812a03fd027b10d5bd3d63ede
mathiasfri <mathiasfridthjof@live.dk>
2023-11-24 14:36:21 +0100

    userSettings fixed

--- pages/userSettings/userSettings.js
+++ pages/userSettings/userSettings.js
@@ -5,34 +5,59 @@ import {
   makeOptions,
 } from "../../utils.js";
 
-const SERVER_URL = API_URL + "/userSettings";
+const SERVER_URL = API_URL + "/user";
 
 export async function initUserSettings(){
     const submitButton = document.getElementById("submit-button");
 
     submitButton.addEventListener("click", async function (event) {
+        let allergies = [];
+        
         const age = document.getElementById("age").value;
         const weight = document.getElementById("weight").value;
         const height = document.getElementById("height").value;
-        const sex = document.getElementById("sex").value;
         const activityLevel = document.getElementById("activity-level").value;
 
+        const allergyContainer = document.getElementById("allergy-container");
+        const allergyInputs =
+          allergyContainer.querySelectorAll('input[type="text"]');
+        allergyInputs.forEach((a) => {
+          if (a.value.trim().length > 0) {
+            allergies.push(a.value);
+          }
+        });
+        
         const updatedUser = {
             age: parseInt(age),
             weight: parseInt(weight),
             height: parseInt(height),
-            sex: sex,
+            allergies: allergies,
             activityLevel: activityLevel
         };
-
-        const options = makeOptions("PUT", updatedUser);
-
-        const response = await fetch(SERVER_URL, options);
-
+        
+        const token = localStorage.getItem("token");
+        const options = makeOptions("PUT", updatedUser, token);
+        const username = localStorage.getItem("user");
+        const response = await fetch(SERVER_URL + "/" + username, options);
+        
         if (response.status === 200) {
             alert("User updated successfully");
         } else {
             handleHttpErrors(response);
         }
     });
+
+    function addAllergy(event) {
+        if (event.target.value.length === 1) {
+          const inputContainer = document.getElementById("allergy-container");
+          const newInput = document.createElement("input");
+          newInput.type = "text";
+          newInput.placeholder = "Enter a preference/allergy";
+          inputContainer.appendChild(newInput);
+          newInput.addEventListener("input", addAllergy);
+        }
+      }
+    document
+    .getElementById("allergy-input")
+    .addEventListener("input", addAllergy);
 }
